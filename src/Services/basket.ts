1468a3a7d9e5664834e1505d19eb8fbd469fc7ce
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-05-06 14:00:42 +0100

    added refreshtoken api that gets called with every api call to check if access token still valid, if not then it creates a new access token

--- src/Services/basket.ts
+++ src/Services/basket.ts
@@ -1,7 +1,17 @@
 import axios from "axios";
 import { url } from "./url";
+import TokenService from "./token";
 
 const getAmountOfItemsInBasket = async (token: string) => {
+
+  const refreshToken: any = localStorage.getItem('refreshToken');
+
+  const checkIfTokenValid = await TokenService.refreshToken(token, refreshToken);
+  if (checkIfTokenValid.data?.access_token) {
+    token = checkIfTokenValid.data.access_token
+  }
+
+
   const res = await axios.get(`${url}/basket/amount`, {
     headers: {
       authorization: `Bearer ${token}`,
@@ -11,6 +21,12 @@ const getAmountOfItemsInBasket = async (token: string) => {
 };
 
 const getMyBasket = async (token: string) => {
+  const refreshToken: any = localStorage.getItem('refreshToken');
+
+  const checkIfTokenValid = await TokenService.refreshToken(token, refreshToken);
+  if (checkIfTokenValid.data.access_token) {
+    token = checkIfTokenValid.data?.access_token
+  }
   const res = await axios.get(`${url}/basket`, {
     headers: {
       authorization: `Bearer ${token}`,
@@ -20,6 +36,12 @@ const getMyBasket = async (token: string) => {
 }
 
 const deleteItemFromBasket = async (token: string, id: string) => {
+  const refreshToken: any = localStorage.getItem('refreshToken');
+
+  const checkIfTokenValid = await TokenService.refreshToken(token, refreshToken);
+  if (checkIfTokenValid.data?.access_token) {
+    token = checkIfTokenValid.data.access_token
+  }
   const res = await axios.delete(`${url}/basket/${id}`, {
     headers: {
       authorization: `Bearer ${token}`,
