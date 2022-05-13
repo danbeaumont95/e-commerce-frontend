d50338a13369f83944f7c9683191b2727205aa6f
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-05-13 16:31:16 +0100

    can now update addresses

--- src/Services/address.ts
+++ src/Services/address.ts
@@ -18,8 +18,24 @@ const getMyAddresses = async (token: string) => {
   return res
 };
 
+const updateMyAddress = async (token: string, details: object, id: string ) => {
+  const refreshToken: any = localStorage.getItem('refreshToken');
+
+  const checkIfTokenValid = await TokenService.refreshToken(token, refreshToken);
+  if (checkIfTokenValid.data.access_token) {
+    token = checkIfTokenValid.data?.access_token
+  }
+  const res = await axios.put(`${url}/address/${id}`, details, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    }
+  });
+  return res
+}
+
 const AddressService = {
-  getMyAddresses
+  getMyAddresses,
+  updateMyAddress
 }
 
 export default AddressService;
