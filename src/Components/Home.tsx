ce285173e5e50acad75b9a2ebbf7caa08fd436d5
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-05-14 21:11:13 +0100

    added button to add item to basket from items list

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import ItemService from '../Services/items';
+import BasketService from '../Services/basket';
 import NavBar from './NavBar';
 import UserById from './UserById';
 import '../Styles/Home.css';
+import Swal from 'sweetalert2';
+import { connect } from "react-redux";
+import * as Types from '../store/types'
+import { Dispatch } from 'redux';
 
 type ItemState = {
   _id: string;
@@ -17,20 +22,48 @@ type State = {
   items: ItemState[]
 }
 
-type Props = {}
+type Props = {
+  updateAmountOfItemsInBasket: any;
+  amountOfItemsInBasket?: number;
+};
 
-export default class Home extends Component<Props, State> {
+class Home extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
       items: []
     }
+    this.addToBasket = this.addToBasket.bind(this)
   }
   componentDidMount() {
     ItemService.getAllItems()
       .then((res) => {
         if (res.data.length) {
           this.setState({items: res.data})
+        }
+      })
+  }
+
+  addToBasket(id: string) {
+    const accessToken: any = localStorage.getItem('accessToken');
+    BasketService.addItemToBasket(accessToken, id)
+      .then((res) => {
+        if (res.data.Message) {
+          BasketService.getMyBasket(accessToken)
+            .then((res) => {
+              const {data} = res;
+              this.props.updateAmountOfItemsInBasket(data.items.length)
+            })
+          return Swal.fire({
+            title: 'Success',
+            text: 'Item added to basket!'
+          })
+        }
+        else {
+          return Swal.fire({
+            title: 'Error',
+            text: 'Please try again'
+          })
         }
       })
   }
@@ -50,6 +83,7 @@ export default class Home extends Component<Props, State> {
               <p>{el.description}</p>
               <h4 style={{color: 'red'}}>£{el.price}</h4>
               <UserById id={el.seller}/>
+              <button onClick={() => this.addToBasket(el._id)}>Add to Basket!</button>
             </div>
           ))}
         </div>
@@ -57,3 +91,15 @@ export default class Home extends Component<Props, State> {
     )
   }
 }
+
+const mapStateToProps = (state: State )=> state;
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+
+  updateAmountOfItemsInBasket: (amountOfItemsInBasket: number) => dispatch({
+    type: Types.UPDATE_AMOUNT_OF_ITEMS_IN_BASKET, payload: {
+      amountOfItemsInBasket
+    }
+  })
+});
+const connectComponent = connect(mapStateToProps, mapDispatchToProps);
+export default connectComponent(Home);
